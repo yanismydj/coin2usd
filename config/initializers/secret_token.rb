b6c6d3ae64e7747b3ec56f24993788149a9a3748
Yan Lhert <info@yanlhert.com>
2013-12-11 12:33:46 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coin2usd::Application.config.secret_key_base = '2ab9656bee2fe4a8956668820cf32d9579f903e5c14f7f9b611678ca97afdb20c7d32fc19dfc3e9a7ac332e5659bb5c39296210a16ebd38521cb2358076c0f69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Coin2usd::Application.config.session_store :cookie_store, key: '_coin2usd_session'
