44e49b292ec72cc5f93f6db628ebd30576ac8dde
Yan Lhert <info@yanlhert.com>
2013-12-11 13:39:13 -0800

    beginning the quote algo

--- app/models/tableless/quote.rb
+++ app/models/tableless/quote.rb
@@ -1,9 +1,19 @@
 class Quote
+  def intialize(quantity, buying = true)
+    if buying
+
+    else # they are selling
+
+    end
+  end
+
   def highest_ask
     bitstamp_order_book_asks.first # ["873.10", "0.05500000"]
   end
 
-  def lowest_bid
+  def lowest_bid 
+    # lowest price someone will buy for. this is what the instructions call 
+    # for but this seems wrong. you'd want the highest price if you were selling
     bitstamp_order_book_bids.last # ["0.01", "1044278.00000000"]
   end
 
